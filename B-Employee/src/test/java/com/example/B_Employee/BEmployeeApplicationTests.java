2dae23872315781242ef4129153a5490911f9ad2
ram_vish Coding Journey <119061245+rvcjourney@users.noreply.github.com>
2025-01-17 22:24:24 +0530

    Add files via upload
    This is Employee Management Sysytem Logical Code written in Java using SpringBoot framework and required dependencies!!

---
+++ B-Employee/src/test/java/com/example/B_Employee/BEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.B_Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
