54a35ba9ade90897f57899dc935bd82ac0539114
ram_vish Coding Journey <119061245+rvcjourney@users.noreply.github.com>
2025-01-25 12:28:19 +0530

    Delete B-Employee directory

--- B-Employee/src/main/java/com/example/B_Employee/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.B_Employee.repository;
-
-import com.example.B_Employee.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
